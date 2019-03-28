1643beb7166ecaa60d711c620c5c1dce751bf2a3
Fikri <jamesfikrii@gmail.com>
2019-03-28 11:27:25 +0700

    2

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var express = require('express')
+var server = require('express')
 var bodyParser = require('body-parser')
-var app = express()
+var app = server()
 var mysql = require('mysql')
+var cors = require('cors')
+
+
+app.use(cors())
 app.use(bodyParser.json());
+
+
 
 const port = 2000 
 const db = mysql.createConnection({
@@ -18,8 +24,8 @@ app.get('/' , (req,res) => {
     res.send('<h1>Hello World</h1>')
 })
 
-app.get('/nama/:id' , (req,res) => {
-    var nama = req.params.id
+app.get('/nama/:terserah' , (req,res) => {
+    var nama = req.params.params
     res.send('nama saya ' + nama)
 })
 
@@ -47,18 +53,21 @@ app.get('/user/:terserah' , (req,res) => {
 app.post('/addtodo' , (req,res) => {
     // REQ.BODY BUAT AMBIL DATA DARI FRONT END
     console.log(req.body)
-    var user = req.body.user;
-    var todo = req.body.todo
-    var sql = `insert into to_do (id_user, to_do) values (${user},'${todo}');`
+    var sql = `insert into to_do (id_user, to_do) values (${req.body.user},'${req.body.todo}');`
     db.query(sql , (err,result) => {
         if(err) throw err
-        res.send('add to do sukses')
+        var sql2 = `select to_do.id, username, to_do from to_do
+                    join users on id_user = users.id;`
+        db.query(sql2, (err,result1)=>{
+            if(err) throw err
+            res.send(result1)
+        })
     })
 })
 
 app.post('/adduser' , (req,res) => {
     var data = req.body
-    var sql = 'insert into users set?;'
+    var sql = 'insert into users set ?;'
     db.query(sql,data, (err,result) => {
         if(err) throw err
         res.send('Add User Sukses')
@@ -67,14 +76,20 @@ app.post('/adduser' , (req,res) => {
 
 app.put('/edittodo/:id' , (req,res) => {
     var id = req.params.id
-    var data = req.body
+    var data = req.body.todo
     var sql = `UPDATE to_do
                SET to_do = ?
                WHERE id = ${id};`
     db.query(sql ,data, (err,result)=>{
         try{
             if(err) throw err.message
-            res.send('berhasil di update')
+            var sql2 = `select to_do.id, username, to_do from to_do
+                        join users on id_user = users.id;`
+            db.query(sql2, (err,result1) => {
+                if(err) throw err
+                res.send(result1)
+            })
+            // res.send('berhasil di update')
         }catch(err){
             res.send(err)
         }
